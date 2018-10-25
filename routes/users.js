ad000608a8bc46c770d92974c002954c60837117
shubhamkr17 <sk767285@gmail.com>
2018-10-25 23:38:17 +0530

    updated users router to add new address and delete address

--- routes/users.js
+++ routes/users.js
@@ -9,16 +9,60 @@ var User = require('../schemas/userSchema');
 router.get('/', function(req, res) {
   User.find({},function (err,user) {
     if(err) throw err;
-    res.json(user)
+    res.send(user);
   });
 });
 
 router.get('/:gid', function(req,res) {
   User.findOne({gid: req.params.gid}, function (err,user) {
     if(err) throw err;
-    res.json(user)
+    res.send(user);
   });
 });
+
+
+//push new address
+router.put('/address/new/:gid',function(req, res) {
+  User.findOne({gid: req.params.gid}, function (err,user) {
+    if(err)
+    {
+      throw err;
+    }
+    else if(user){
+      user.address.push(req.body.address);
+      user.save();
+      res.send(user);
+    }
+    else {
+      res.status(404).send("Record does not exist!");
+    }
+  });
+});
+
+//delete address by _id
+router.put('/address/delete/:gid/:_id',function(req, res) {
+  User.findOne({gid: req.params.gid}, function (err,user) {
+    if(err)
+    {
+      throw err;
+    }
+    else if(user){
+      user.address.forEach(function(element){
+        if(element._id == req.params._id)
+        {
+          const index = user.address.indexOf(element);
+          user.address.splice(index,1);
+          user.save();
+          res.send(user);
+        }
+      });
+    }
+    else {
+      res.status(404).send("Record does not exist!");
+    }
+  });
+});
+
 
 
 
@@ -37,12 +81,12 @@ router.post('/',(req,res)=>{
     isSupplier : req.body.isSupplier
 
   });
-  
+
   const result =  user.save();
 
-  res.json(user);
+  res.send(user);
   debug(user);
-  
+
 });
 
 
