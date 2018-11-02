8761f7ea6e413755135cedb10923ee6f7c0c2bca
shazz10 <sksuraj2136@gmail.com>
2018-11-02 22:00:59 +0530

    Shazam

--- routes/prescriptions.js
+++ routes/prescriptions.js
@@ -5,37 +5,36 @@ var Prescription = require('../schemas/prescriptionSchema');
 var Retailer = require('../schemas/retailerSchema');
 
 /* GET users listing. */
-router.get('/',async (req,res)=>{
-  //console.log(req.query);
-  const prescriptions = await Prescription.find({
-    address.pincode : parseInt(req.query.pincode)
+router.get('/', function(req, res) {
+  Prescription.find({},function (err,prescription) {
+    if(err) throw err;
+    res.send(prescription);
   });
-  res.send(prescriptions);
 });
 
-router.post('/',(req,res)=>{
-  const prescription = new Prescription({
-    patient_gid : req.body.patient_gid,
-    prescription_link : req.body.prescription_link,
-    comment: req.body.comment,
-    address : req.body.address
-  });
-  const result= prescription.save();
+// router.post('/',(req,res)=>{
+//   const prescription = new Prescription({
+//     patient_gid : req.body.patient_gid,
+//     prescription_link : req.body.prescription_link,
+//     comment: req.body.comment,
+//     address : req.body.address
+//   });
+//   const result= prescription.save();
 
-  const retailers = await Retailer.find({
-    address.pincode : req.body.address.pincode,
-  });
+//   const retailers = await Retailer.find({
+//     address.pincode : req.body.address.pincode,
+//   });
 
-  retailers.forEach(function(element){
-      element.available_prescriptions.push(prescription.patient_gid);
-      element.save();
-      });
+//   retailers.forEach(function(element){
+//       element.available_prescriptions.push(prescription.patient_gid);
+//       element.save();
+//       });
 
-  res.json(prescription);
-  debug(prescription);
-});
+//   res.json(prescription);
+//   debug(prescription);
+// });
 
-router.get()
+// router.get()
 
 
 module.exports = router;
